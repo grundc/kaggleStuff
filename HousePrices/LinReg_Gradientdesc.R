34f50abc095948adfe1ce3432b9439964fed4960
grundc <christoph.grund@web.de>
2017-08-28 21:08:43 +0200

    Done first prediction

--- HousePrices/LinReg_Gradientdesc.R
+++ HousePrices/LinReg_Gradientdesc.R
@@ -1,5 +1,14 @@
 # LinReg_GradientDesc
 
+### Debugging (with theta = 0)
+# X_debug <- matrix(c(1,1,1,1,5,2,4,5), nrow=4, ncol=2)
+# y_debug <- matrix(c(1,6,4,2))
+# LinReg_GradientDesc(X_debug,y_debug,0.01,0,1000,0.0001)
+### Result
+#### [,1]
+#### [1,]  5.2147549
+#### [2,] -0.5733459
+
 LinReg_GradientDesc <- function(X, y, adjust,lambda, maxRepeats, minCostDiff)  {
       
      
@@ -8,7 +17,7 @@ LinReg_GradientDesc <- function(X, y, adjust,lambda, maxRepeats, minCostDiff)  {
       m <- dim(X)[1]
       
       # Setting the initial thetas all to ones
-      theta <- as.matrix(rep(1,dim(X)[2]))
+      theta <- as.matrix(rep(0,dim(X)[2]))
       
       h0 <- X %*% theta
       cost_old <- 1/ (2*m) * sum((h0 - y)^2)
--- HousePrices/explore_data.R
+++ HousePrices/explore_data.R
@@ -1,21 +1,25 @@
 
 
-setwd("./R/kaggle/HousePrices")
-
-
-data <- "./data/train.csv"
-
-train_data <- read.csv(data)
-
-summary(train_data)
-dim(train_data)
-
-
+setwd("./GitHub/kaggleStuff/HousePrices")
 
 library(data.table)
 
-train_data <- data.table(train_data)
+# Loading data
+train_csv <- "./data/train.csv"
+test_csv <- "./data/test.csv"
+result_csv <- "./data/myResult.csv"
 
+train_data <- read.csv(train_csv)
+test_data <- read.csv(test_csv)
+
+train_data <- data.table(train_data)
+test_data <- data.table(test_data)
+
+
+
+
+summary(train_data)
+dim(train_data)
 
 # LotArea (Grundstück SF) vs price
 train_data_subset <- train_data[LotArea <= 50000,.(LotArea, SalePrice)]
@@ -52,7 +56,23 @@ plot(train_data_subset)
 
 # First try
 train_data_subset <- train_data[, .(SalePrice, HouseAge = YrSold - YearBuilt, LotArea, TotalHouseSF = GrLivArea+X1stFlrSF+X2ndFlrSF, OverallCond)]
+test_data_subset <- test_data[, .(Id, HouseAge = YrSold - YearBuilt, LotArea, TotalHouseSF = GrLivArea+X1stFlrSF+X2ndFlrSF, OverallCond)]
 
+library(caret)
+
+modelFit <- train(SalePrice~.,data=train_data_subset,method="lm")
+modelFit$finalModel
+predictions <- predict(modelFit,newdata=test_data_subset)
+length(predictions)
+result <- cbind(1461:(1460 + length(predictions)),predictions)
+colnames(result) <- c("Id","SalePrice")
+head(result)
+write.csv(result,file=result_csv, quote=FALSE, row.names = FALSE)
+
+
+
+
+# Alternative methods
 X <- as.matrix(train_data[, .(bias = 1,HouseAge = YrSold - YearBuilt, LotArea, TotalHouseSF = GrLivArea+X1stFlrSF+X2ndFlrSF, OverallCond)])
 
 y <- as.matrix(train_data[,SalePrice])
@@ -65,11 +85,17 @@ lm_houseprice_1 <- lm(SalePrice~., train_data_subset)
 summary(lm_houseprice_1)
 
 
-scaled.X <- scale(X[,2:5])
-scaled.X
+
+scaled.X <- cbind(X[,1],scale(X[,2:5]))
+scaled.X 
 colMeans(scaled.X)
 
-GradientCalculation <- LinReg_GradientDesc(X,y,0.000000005,0,1000,0.0001)
+
+# Normal equation   (X(T) * X)^-1 * X(T)*y
+solve(t(scaled.X) %*% scaled.X) %*% t(scaled.X) %*% y
+
+LinReg_GradientDesc(scaled.X,y,0.001,0,100000,0.0001)
+
 
 
 
